a9c88ce589bc872d33b2c77c50d49e03ab5116b8
Lauro <hey@laurosilva.com>
2022-12-01 16:23:57 -0800

    update folder name

--- runner.js
+++ runner.js
@@ -1,22 +1,24 @@
-const spawn = require('cross-spawn');
-const { readdirSync, fstat, statSync } = require('fs')
+const spawn = require('cross-spawn')
+const {readdirSync, fstat, statSync} = require('fs')
 const path = require('path')
 const prompts = require('prompts')
 const getLessons = () => {
-    return readdirSync(path.resolve(__dirname)).filter(path => statSync(path).isDirectory()).filter(d => d.includes('leccion'))
+  return readdirSync(path.resolve(__dirname))
+    .filter(path => statSync(path).isDirectory())
+    .filter(d => d.includes('leccion'))
 }
 
 const main = async () => {
-    const lessons = getLessons()
-    const response = await prompts({
-        type: 'select',
-        choices: lessons.map(item => ({ title: item, value: item })),
-        name: 'lesson',
-        message: 'What lesson you want to run?',
-    })
-    const { lesson } = response
+  const lessons = getLessons()
+  const response = await prompts({
+    type: 'select',
+    choices: lessons.map(item => ({title: item, value: item})),
+    name: 'lesson',
+    message: '¿Qué lección quieres ejecutar?',
+  })
+  const {lesson} = response
 
-    spawn.sync('npm', ['run', '--prefix', lesson, 'dev'], { stdio: 'inherit' })
+  spawn.sync('npm', ['run', '--prefix', lesson, 'dev'], {stdio: 'inherit'})
 }
 
 main()
